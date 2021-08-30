edcd3eebf465684e692ae52d2c75cff8bf8c1fdf
Your Name <ishitanarang28@gmail.com>
2021-08-30 22:14:48 +0530

    first commit

--- webapp/models.py
+++ webapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     creator=models.CharField(max_length=20,null=True)
     description=models.CharField(max_length=100)
     comments=models.CharField(max_length=50)
 
+    
     def _str_(self):
         return self.creator
 
--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .import views
 
-urlpatterns={
+urlpatterns=[
     path('blog/',views.blog,name='blog-home'),
-}
+]
